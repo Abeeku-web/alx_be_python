ca79026eae75ccfd78332e33232e4a1d22927488
abeekuhag <abee.hagan@gmail.com>
2024-06-22 03:37:02 +0300

    Created script to perform arithmetic operations on number1 and number2

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+sum_number1_number2 = number1 + number2
+#sum_number1_number2 adds the two variables
+diff_number1_number2 = number1 - number2
+#diff_number1_number2 finds the different b/n the two variables
+prod_number1_number2 = number1 * number2
+#prod_number1_number2 finds the of the two variables
+print ("Addition of", number1, " and ", number2, " is ", sum_number1_number2)
+print ("Subtraction of", number1, " and ", number2, " is ", diff_number1_number2)
+print ("Multiplication of", number1, " and ", number2, " is ", prod_number1_number2)
