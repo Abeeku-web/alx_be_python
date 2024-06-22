e2d73073c228e527997ba016bef8fdbf13a537ec
abeekuhag <abee.hagan@gmail.com>
2024-06-22 04:20:51 +0300

    Calculated simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+principal = 1000
+#principal value is in $
+rate = 0.05
+#rate represents the annual interest rate
+time = 3
+#time is measured in years
+interest = principal * rate * time
+print ("The simple interest is:", interest)
