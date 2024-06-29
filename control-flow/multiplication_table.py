bd225c633812808d965e5733245135999ed20303
abeekuhag <abee.hagan@gmail.com>
2024-06-29 06:23:29 +0300

    Added spaces in table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,5 +2,5 @@ number = int(input("Enter a number to see its multiplication table:"))
 
 for i in range(1,11):
     multiplication = number * i
-    print(f"{number}*{i}={multiplication}")
+    print(f"{number} * {i} = {multiplication}")
     i +=1
