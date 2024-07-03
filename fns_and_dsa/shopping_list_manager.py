ef33968fb533c9a5f043167e3634b800d4afcc56
abeekuhag <abee.hagan@gmail.com>
2024-07-03 18:26:24 +0300

    edit

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,15 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice:")
+
+        if choice == "1":
+            prompt = input("Please select an item to add to your cart:")
