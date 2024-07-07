34665b77bc889844a50e7ce99dc1482d6ae0e8fb
abeekuhag <abee.hagan@gmail.com>
2024-07-07 23:09:51 +0000

    tryr

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,23 +1,45 @@
 ["num1, num2, operation"]
 
 def perform_operation(num1,num2,operation):
+
+    if operation == "add":
+        sum = num1 + num2
+        print(sum)
+    elif operation == "subtract":
+        difference = num1 - num2
+        print(difference)
+    elif operation == "multiply":
+        product = num1 * num2
+        print(product)
+    elif operation == "divide":
+        try:
+            division = num1/num2
+            print(division)
+        except ZeroDivisionError:
+            print("Cannot divide by zero")
+    else:
+        print("Invalid opertion entered, please try again.")
+
+
     
-    match operation:
-        case "add":
-            sum = num1 + num2
-            print(sum)
-        case "subtract":
-            difference = num1 - num2
-            print(difference)
-        case "multiply":
-            product = num1 * num2
-            print(product)
-        case "divide":
-            try:
-                division = num1/num2
-                print(division)
-            except ZeroDivisionError:
-                print("Cannot divide by zero")
-perform_operation(10,5,"divide")
+    
+#     match operation:
+#         case "add":
+#             sum = num1 + num2
+#             print(sum)
+#         case "subtract":
+#             difference = num1 - num2
+#             print(difference)
+#         case "multiply":
+#             product = num1 * num2
+#             print(product)
+#         case "divide":
+#             try:
+#                 division = num1/num2
+#                 print(division)
+#             except ZeroDivisionError:
+#                 print("Cannot divide by zero")
+# perform_operation(10,5,"divide")
+
 
  
